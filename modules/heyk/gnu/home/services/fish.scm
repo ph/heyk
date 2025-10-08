535edc7aaad44087683641e9dc0e9f5e458910ed
Pier-Hugues Pellerin <phpellerin@gmail.com>
2025-10-08 11:17:01 -0400

    feat: add fish hydro service

---
+++ modules/heyk/gnu/home/services/fish.scm
@@ -0,0 +1,33 @@
+(define-module (heyk gnu home services fish)
+  #:use-module (gnu home services)
+  #:use-module (guix packages)
+  #:use-module (guix gexp)
+  #:use-module (gnu services configuration)
+  :export (home-fish-hydro-service-type
+	   home-fish-hydro-configuration))
+
+(define-configuration/no-serialization home-fish-hydro-configuration
+  (fish-hydro
+   (package fish-hydro)
+   "The fish-hydro package to use"))
+
+(define (add-fish-hydro-configuration config)
+  `(("fish/conf.d/hydro.fish" ,(file-append (home-fish-hydro-configuration-fish-hydro config) "/share/conf.d/hydro.fish"))
+    ("fish/functions/fish_mode_prompt.fish" ,(file-append (home-fish-hydro-configuration-fish-hydro config) "/share/functions/fish_mode_prompt.fish"))
+    ("fish/functions/fish_prompt.fish" ,(file-append (home-fish-hydro-configuration-fish-hydro config) "/share/functions/fish_prompt.fish"))))
+
+(define (home-fish-hydro-profile-service config)
+  (list (home-fish-hydro-configuration-fish-hydro config)))
+
+(define home-fish-hydro-service-type
+  (service-type
+   (name 'home-fish-hydro-service-type)
+   (extensions
+    (list
+     (service-extension home-xdg-configuration-files-service-type
+			add-fish-hydro-configuration)
+     (service-extension home-profile-service-type
+			home-fish-hydro-profile-service)))
+   (description "Configure fish hydro prompt")
+   (default-value (home-fish-hydro-configuration))))
+
